043e4104eec4be1ae8c3403e97e58768e4b108ff
Thomas Hjelm <thomas@azabua.com>
2014-10-06 23:35:26 -0500

    Added buttons for operators and numbers and hooked up click events to give them a selected class when clicked.

--- arithmetic.js
+++ arithmetic.js
@@ -2,14 +2,43 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault("counter", 0);
 
-  Template.hello.helpers({
+  Template.config.helpers({
+    class: function(id) {
+      return Session.get(id) || "";
+    },
     counter: function () {
       return Session.get("counter");
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
+  Template.config.events({
+    'click #operators > button': function(evt) {
+      var id = evt.currentTarget.id;
+
+      Session.set('add', "");
+      Session.set('subtract', "");
+      Session.set('multiply', "");
+      Session.set('divide', "");
+      Session.set(id, "selected");
+      Session.set('operator', id);
+    },
+    'click #numbers > button': function(evt) {
+      var id = evt.currentTarget.id;
+
+      Session.set('one', '');
+      Session.set('two', '');
+      Session.set('three', '');
+      Session.set('four', '');
+      Session.set('five', '');
+      Session.set('six', '');
+      Session.set('seven', '');
+      Session.set('eight', '');
+      Session.set('nine', '');
+      Session.set('ten', '');
+      Session.set(id, 'selected');
+      Session.set('number', id);
+    },
+    'click #btn-add': function () {
       // increment the counter when button is clicked
       Session.set("counter", Session.get("counter") + 1);
     }
