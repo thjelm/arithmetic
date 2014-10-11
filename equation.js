300c51a0f7b418324e195b5db12bc9bdae1fd343
Thomas Hjelm <thomas@azabua.com>
2014-10-11 01:28:32 -0500

    fixed subtraction and division equations.  Before was producing equations like "1 - 5" and "2 / 5".  We really want the 'other number' to be the large answer of the corresponding addition/multiplication problem.

--- equation.js
+++ equation.js
@@ -6,6 +6,14 @@ generateEquation = function(operator, number) {
 	var other_number = randomIntFromInterval(1,9),
 		number_pos = randomIntFromInterval(0,1);
 
+	if (operator === '-') {
+		other_number = parseInt(number) + other_number;
+		number_pos = 1;
+	} else if (operator === "\u00F7") {
+		other_number = parseInt(number) * other_number;
+		number_pos = 1;
+	}
+
 	if (number_pos) {
 		return other_number + ' ' + operator + ' ' + number + ' = ______';
 	} else {
